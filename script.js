c87284fcd3239c094b37451b66c2fa434c0ff890
Gabriele <gabrielepese@gmail.com>
2020-07-01 15:11:52 +0200

    x simo

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
-var numeriComputer = []
-for (var i = 0; i < 4; i++) {
-  var numero = numeroRandom(1, 100)
-  numeriComputer.push(numero)
-
-}
-console.log(numeriComputer)
-
-
 // I numeri non possono essere duplicati
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+var arrayComputer = []
+var i= 0
+while ( i < 4) {
+  var numeroRand = numeroRandom(1, 100)
 
-var numeroUtente = parseInt(prompt("dammi numero 1-100"))
+  if (checkNumero(arrayComputer, numeroRand) != true) {
+    arrayComputer.push(numeroRand)
+  }
+i++
+}
+
+console.log("arraycomputer = " + arrayComputer)
+
+
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+var maxTentativi = 3
+var arrayUtente = []
+
+
+while (numeroUtente <= maxTentativi && checkNumero (arrayComputer, numeroUtente) != true) {
+
+  arrayUtente.push(numero)
+}
+function numero (numero){
+  var numeroUtente = parseInt(prompt("dammi numero 1-100"))
+  return numeroUtente
+}
+
+
+console.log("array utente = " + arrayUtente);
 
 // L’utente non può inserire più volte lo stesso numero.
 
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-
+var trovato = false
 var i=0
-while (i < numeriComputer.length && trovato = false) {
-  var check = checkNumero (numeriComputer , numeroUtente)
+while (i < arrayComputer.length && trovato == false) {
+  var check = checkNumero (arrayComputer , numeroUtente)
    i++
+
+   if (check == true) {
+     console.log("TROVATO - FINE");
+   }
+   else {
+     var numeroUtente = parseInt(prompt("dammi numero 1-100"));
+   }
+
 }
-
-
-
 
 function checkNumero (array, numeroInserito){
   var i= 0
   var trovato = false
-  while (i < array.length && trovato = false) {
+  while (i < array.length && trovato == false) {
     if (array[i] == numeroInserito) {
     trovato = true
   }
